21cf5ed319bfb4ec2a00db8f53e447c1fb8586b1
_MadushaSandaruwan <madushakv@live.com>
2023-09-30 02:56:04 +0000

    fixed images and made changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Madusha Sandaruwan",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "madushakv@live.com",
+  gitHub: "madushas",
+  instagram: "",
+  linkedIn: "madushasandaruwan",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "madushas",
+  youTube: "madushasandaruwan",
 };
 
 const primaryColor = "#4E567E";
